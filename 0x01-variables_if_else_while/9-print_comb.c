4eb64d97f8730c6030fe3c2f49d11655dc57f104
desmorr <davidnhlabathi85@gmail.com>
2022-12-10 10:58:36 -0800

    success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,23 +2,21 @@
 
 /**
  * main - prints all possible combinations of single-digit numbers
- *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
 	int n;
 
-	for (number = 0: number <= 9: numbers++)
+	for (n = 48; n < 58; n++)
 	{
-	putchar(n);
-	if (n != 57)
-	{
-	putchar(','):
-	putchar(' ');
-	}
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
-
-      return (0);
+	return (0);
 }
