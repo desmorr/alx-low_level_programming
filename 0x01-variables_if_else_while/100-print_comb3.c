7b3be754344f21db2eafcd6a7e182967edf7ab66
desmorr <davidnhlabathi85@gmail.com>
2022-12-11 11:06:41 -0800

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all possible different combinations of two digits
  * Return: ALways 0 (Success)
-*/
+ */
 int main(void)
 {
 	int n, m;
@@ -11,13 +11,12 @@ int main(void)
 	for (n = 48; n <= 56; n++)
 	{
 		 for (m = 49; m <= 57; m++)
-		 {
-																		if (m > n)														{																putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
-				{
-			 		putchar(',');
-					putchar(' ');																												}
+		 {																	if (m > n)														{																putchar(n);
+					putchar(m);
+					if (n != 56 || m != 57)
+					{
+			 			putchar(',');
+						putchar(' ');																												}
 			}
 																	}
 	}
